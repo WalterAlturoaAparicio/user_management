42e4204e26dbbf1425d1ab8cbcdd71e8e7d69d43
Walter Alturo <walteralturoaparicio@gmail.com>
2025-01-28 20:02:23 -0500

    simple login

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Data:', this.loginForm.value);
+    }
+
+    if (isPlatformBrowser(this.platformId)) {
+      // Lógica dependiente del DOM solo cuando se ejecuta en el navegador
+      console.log('El formulario ha sido enviado');
+    }
+  }
 }
